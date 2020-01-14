e6c59180adfb5006fcb8deb9ed91d7e729b086f2
adhuliya <dhuliya@cse.iitb.ac.in>
2020-01-14 14:12:05 +0530

    semi-automated-update

---
+++ before_commit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# copy relevant files into the repo
+DEST_FOLDER=llvm-clang9.0.0/llvm
+SRC_FOLDER=$MYDATA/local/packages-live/$DEST_FOLDER;
+
+mkdir -p DEST_FOLDER;
+cp -R $SRC_FOLDER/tools/clang/lib/StaticAnalyzer/Checkers/SlangCheckers $DEST_FOLDER;
+cp $SRC_FOLDER/tools/clang/include/clang/StaticAnalyzer/Checkers/Checkers.td $DEST_FOLDER;
+cp $SRC_FOLDER/tools/clang/lib/StaticAnalyzer/Checkers/CMakeLists.txt $DEST_FOLDER;
+
+
